e7e20c069c16eb38acb5de22e4f33f79c77b72ab
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-02-12 14:31:43 -0600

    creating questions for array

---
+++ index.js
@@ -0,0 +1,58 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+       type:"input",
+       name:"Project Title",
+       message:"What is the your project's title?", 
+    },
+    {
+        type:"input",
+        name:"Installation",
+        message:"How do you install your application?", 
+     },
+     {
+        type:"input",
+        name:"Instructions",
+        message:"What instructions does the user need to follow?", 
+     },
+     {
+        type:"input",
+        name:"Credits",
+        message:"Do you have any credits to add?", 
+     },
+     {
+        type:"input",
+        name:"Usage",
+        message:"How to use this app?", 
+     },
+     {
+        type:"input",
+        name:"License",
+        message:"What liscense did you use?", 
+     },
+     {
+        type:"input",
+        name:"GitHub",
+        message:"GitHub username: ",
+     },
+     {
+        type:"input",
+        name:"E-mail",
+        message:
+     }
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
