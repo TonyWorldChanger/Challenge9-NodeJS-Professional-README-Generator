e357d65b1715b27d86a1cb0158c163e007523b01
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-02-12 15:04:23 -0600

    creating function for to write readme file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Challenge9-NodeJS-Professional-README-Generator
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Credit
+Javier Perez YouTube Channel
+DopeDev YouTube Channel
+Cameron Verver TA
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
+const generateMarkdown =require("./utils/generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
@@ -43,13 +45,15 @@ const questions = [
      {
         type:"input",
         name:"E-mail",
-        message:
-     }
+        message:"Email",
+     },
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
